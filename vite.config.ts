6421dc187ec5fc496baf0ff880557c9d363dfeab
RenoR <renu711@hotmail.de>
2025-11-12 14:55:22 +0100

    API Key

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Travel-Planner1/',
+  base: '/Travel-Planner/',
   optimizeDeps: {
     include: ['react-datepicker'],
   },
